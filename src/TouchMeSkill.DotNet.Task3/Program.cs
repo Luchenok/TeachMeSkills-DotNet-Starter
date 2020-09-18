a4d0821d45bae73b5c3dcb21608c99a67700e143
Mihail <70281322+Luchenok@users.noreply.github.com>
2020-09-18 21:25:57 +0300

    feat: add sulution of 3 task

---
+++ src/TouchMeSkill.DotNet.Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TouchMeSkill.DotNet.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal pi = 3.141592653M;
+            decimal r = 15.6M;
+            Console.WriteLine(pi * r * r);
+            Console.ReadKey();
+        }
+    }
+}
