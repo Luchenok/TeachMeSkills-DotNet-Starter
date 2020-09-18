f66e00719dc164b4036e121b2e43875b1f6c9f7c
Mihail <70281322+Luchenok@users.noreply.github.com>
2020-09-18 21:48:23 +0300

    feat: add solution of 5 task

---
+++ src/TouchMeSkill.DotNet.Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TouchMeSkill.DotNet.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 7;
+            double b = 3;
+
+            Console.WriteLine("a * b = " + (a * b));
+            Console.WriteLine("a + b = " + (a + b));
+            Console.WriteLine("a - b = " + (a - b));
+            double c = (a / b);
+            Console.WriteLine("a / b = " + c);
+            Console.ReadLine();
+        }
+    }
+}
