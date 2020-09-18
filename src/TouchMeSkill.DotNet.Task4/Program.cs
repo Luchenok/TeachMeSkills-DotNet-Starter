8d170254df5e9430145cb983e777471cf5a945d2
Mihail <70281322+Luchenok@users.noreply.github.com>
2020-09-18 21:35:57 +0300

    feat: add solution of 4 task

---
+++ src/TouchMeSkill.DotNet.Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TouchMeSkill.DotNet.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pi = 3.141592653M;
+            decimal r = 2.54M;
+            decimal h = 3.0M;
+
+            Console.WriteLine("S = " + (2 * pi * r * (r + h)));
+            Console.WriteLine("V = " + (pi * r * r * h));
+            Console.ReadLine();
+        }
+    }
+}
