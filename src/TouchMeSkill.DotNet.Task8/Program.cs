e320ea0f377aabdf5d5dda55080caf34ce35198b
Mihail <70281322+Luchenok@users.noreply.github.com>
2020-09-25 21:57:30 +0300

    feat: add solution of 9 task

---
+++ src/TouchMeSkill.DotNet.Task8/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TouchMeSkill.DotNet.Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I am a 10 word translator about weather. So far, only words are in my stock: sun, wind, rain, snow, hail, cloudy, ice, warm, cold, disgusting.");
+            Console.Write("Enter a word for translation into Russian:"); 
+            string userWord = Console.ReadLine().ToLower();
+            bool IKnow;
+
+            if (userWord == "sun")
+            {
+                Console.WriteLine("Солнце");
+            }
+            if (userWord == "wind")
+            {
+                Console.WriteLine("Ветер");
+            }
+            if (userWord == "rain")
+            {
+                Console.WriteLine("Дождь");
+            }
+            if (userWord == "snow")
+            {
+                Console.WriteLine("Снег");
+            }
+            if (userWord == "hail")
+            {
+                Console.WriteLine("Град");
+            }
+            if (userWord == "cloudy")
+            {
+                Console.WriteLine("Облачно");
+            }
+            if (userWord == "ice")
+            {
+                Console.WriteLine("Лёд");
+            }
+            if (userWord == "warm")
+            {
+                Console.WriteLine("Тёплый");
+            }
+            if (userWord == "cold")
+            {
+                Console.WriteLine("Холодный");
+            }
+            if (userWord == "disgusting")
+            {
+                Console.WriteLine("Мерзко");
+            }
+            else
+            {
+                Console.WriteLine("I don't know such a word yet");
+            }
+        }
+    }
+}
---
+++ src/TouchMeSkill.DotNet.Task9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TouchMeSkill.DotNet.Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 0;
+            var c = 0;
+            Console.Write("Введите число: ");
+            bool canPars = int.TryParse(Console.ReadLine(), out c);
+            a = c;
+            if (canPars) 
+            {
+                double b = a % 2;
+                if (b != 0)
+                {
+                    Console.WriteLine("Число нечётное!");
+                }
+                else
+                {
+                    Console.WriteLine("Число чётное!");
+                }
+            }
+            if (canPars)
+            {
+                double x = Convert.ToDouble(a) / 2.0;
+                if (x != 0)
+                {
+                    Console.WriteLine("Число нечётное!");
+                }
+                else
+                {
+                    Console.WriteLine("Число чётное!");
+                }
+            }
+        }
+    }
+}
